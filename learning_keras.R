244662782e7620e9933fe1496d410c52e68ae5a7
hk21042 <43421964+hk21042@users.noreply.github.com>
2019-04-29 16:59:26 -0400

    trying keras tutorial
    notes: need to fix errors in regards to downloading tensorflow and anaconda - conda.exe and conda environments are acting weird

---
+++ learning_keras.R
@@ -0,0 +1,72 @@
+install.packages("keras")
+library("keras")
+install_keras() #downloads Keras library and TensorFlow backend?
+Sys.setenv(RETICULATE_PYTHON="C:\\Users\\kimh2\\anaconda3\\python.exe")
+install_keras(conda = "C:\\Users\\kimh2\\anaconda3\\Scripts\\conda.exe")
+#(need anaconda in order to download and run code)
+#anaconda is acting up and so is miniconda ("Can\'t connect to HTTPS URL because the SSL module is not available.")
+#find out what's wrong :(
+
+#using the classic MNIST handwriting dataset
+mnist <- dataset_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+# reshape - turns 3d arrays into matrices (flatten images into vectors)
+dim(x_train) <- c(nrow(x_train), 784)
+dim(x_test) <- c(nrow(x_test), 784)
+# rescale - convert grayscale values to 0~1
+x_train <- x_train / 255
+x_test <- x_test / 255
+
+#one-hot encode vectors into binary class matrices??
+#i guess it basically makes it possible to use ML on the input, and then later revert
+  #the data type to the original??
+  #ref: https://www.quora.com/What-is-one-hot-encoding-and-when-is-it-used-in-data-science
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
+
+#creates a sequential model - stacks of layers
+#each pipe (%>%) adds a new layer to the model
+model <- keras_model_sequential() 
+model %>% 
+  layer_dense(units = 256, activation = "relu", input_shape = c(784)) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 128, activation = "relu") %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = "softmax")
+
+summary(model)
+
+#compile the model with loss function, optimizer, and metrics...
+model %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_rmsprop(),
+  metrics = c("accuracy")
+)
+
+#train with 30 epochs and batches of 128
+history <- model %>% fit(
+  x_train, y_train, 
+  epochs = 30, batch_size = 128, 
+  validation_split = 0.2
+)
+
+plot(history)
+
+#see performace on test data
+model %>% evaluate(x_test, y_test,verbose = 0)
+#$loss
+#$acc
+
+#predictions on new data?
+model %>% predict_classes(x_test)
+
+
+
+
+
+
+
