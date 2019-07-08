b197161bfc8aacf0e92642854145248f1d27617c
kimh2 <Hannah.Kim@jhuapl.edu>
2019-07-08 17:03:50 -0400

    started pr/roc (see descrip)
    started creating code to calculate roc and prediction-recall curves based off my elm data. code is starting off okay, but throws errors because the elm prediction model is returning all 0s for some reason. to do: find out why the model is only predicting 0s and fix it, and apply roc/pr to svm?

---
+++ ROC_PR.R
@@ -0,0 +1,54 @@
+#install.packages("PRROC")
+library("PRROC")
+
+fg <- rnorm(300)
+bg <- rnorm(500, -2)
+
+roc <- roc.curve(scores.class0 = fg, scores.class1 = bg, curve = TRUE)
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = TRUE)
+
+#scores.class0 = classification scores of all data points OR data points in positive class
+#scores.class1 = either same as scores.class0 OR scores for negative data poitnts
+
+plot(roc)
+plot(pr)
+
+x <- c(fg, bg)
+lab <- c(rep(1,length(fg)),rep(0,length(bg)))
+
+roc1 <- roc.curve(scores.class0 = x, weights.class0 = lab, curve = TRUE)
+pr1 <- pr.curve(scores.class0 = x, weights.class0 = lab, curve = TRUE)
+
+wfg <- c(runif(300,min=0.5,max=1),runif(500,min=0,max=0.5))
+wroc <- roc.curve(scores.class0=x,weights.class0=wfg,curve=TRUE)
+wpr <- pr.curve(scores.class0=x,weights.class0=wfg,curve=TRUE)
+
+wroc1 <- roc.curve(scores.class0=x,scores.class1=x,weights.class0=wfg,
+                   weights.class1=1-wfg,curve=TRUE)
+wpr1 <- pr.curve(scores.class0=x,scores.class1=x,weights.class0=wfg,
+                weights.class1=1-wfg,curve=TRUE)
+
+wroc2 <- roc.curve(scores.class0=x,weights.class0=wfg,curve=TRUE,
+                   max.compute=T,min.compute=T,rand.compute=T)
+wpr2 <- pr.curve(scores.class0=x,weights.class0=wfg,curve=TRUE,
+                 max.compute=T,min.compute=T,rand.compute=T)
+
+plot(wpr2,max.plot=TRUE,min.plot=TRUE,rand.plot=TRUE,fill.area=TRUE)
+
+y<-c(rnorm(300,sd=2),rnorm(500,-5,sd=2))
+wroc3 <- roc.curve(scores.class0=y,weights.class0=wfg,curve=TRUE,
+                   max.compute=TRUE,min.compute=TRUE,rand.compute=TRUE)
+wpr3 <- pr.curve(scores.class0=y,weights.class0=wfg,curve=TRUE,
+                 max.compute=TRUE,min.compute=TRUE,rand.compute=TRUE)
+
+plot(wpr2,max.plot=TRUE,min.plot=TRUE,rand.plot=TRUE,fill.area=T,
+     color=2,auc.main=FALSE)
+plot(wpr3,add=TRUE,color=3)
+
+#just playing with the plot at this point tbh
+plot(wpr2,scale.color=heat.colors(100))
+plot(wpr2,color=6,lty="dotted")
+plot(wpr2,legend=1) #1=bottom, 2=left, 3=top, 4=right
+
+
+
--- wildfire_elm.R
+++ wildfire_elm.R
@@ -3,30 +3,140 @@
 library("elmNNRcpp")
 
 setwd("/Users/hk/Desktop/School/MRHS/11th Grade/R/NN-ML/Wildfire-NN-ML")
-data <- read.csv("ml_dly_cal_r3.sel.csv")[,c(1:40,43)]
+data <- read.csv("/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data/ml_dly_cal_r1.sel.csv")[,c(1:41)]
 data <- data[,-c(1,2,3)]
 
 lastVar <- ncol(data)-1
 predictVar <- ncol(data)
 
 for (i in 1:nrow(data)){ #differentiate between "lots" of fires and less fires
-  if (data[i,predictVar]>=1.5){
+  if (data[i,predictVar]>=15){
     data[i,predictVar] <- 1
   } else {
     data[i,predictVar] <- 0
   }
 }
 
-x_train <- data[1:250, 1:lastVar]
-x_test <- data[251:nrow(data), 1:lastVar]
+data <- rbind(data[sample(which(data$fpc1==0),length(which(data$fpc1==1))),],
+              data[which(data$fpc1==1),])
 
-y_train= matrix(data[1:250, predictVar], nrow = length(data[1:250, lastVar]),
+x_train <- data[1:(nrow(data)*0.75), 1:lastVar]
+x_test <- data[(nrow(data)*0.75+1):nrow(data), 1:lastVar]
+
+y_train= matrix(data[1:(nrow(data)*0.75), predictVar], nrow = length(data[1:(nrow(data)*0.75), lastVar]),
              ncol = 1)
 
 fit_elm = elm_train(as.matrix(x_train), y_train, nhid = 1000, actfun = 'purelin',
                     init_weights = "uniform_negative", bias = TRUE, verbose = T)
 
-predict_elm = elm_predict(fit_elm, as.matrix(x_test))
+y_test = data[(nrow(data)*0.75+1):nrow(data), predictVar]
+
+plot_values <- data.frame(matrix(ncol=2))
+colnames(plot_values) <- c("V1", "V2")
+#PR
+for (i in 0:17){
+  predict_elm = elm_predict(fit_elm, as.matrix(x_test))
+  threshold <- 0.1+i*0.05
+  predict_elm <- ifelse(predict_elm<threshold,0,1)
+  xtab <- table(y_test, predict_elm)
+  #print(as.matrix(xtab))
+  df <- as.data.frame(as.matrix(xtab))
+  plot_values <- rbind(plot_values,calc_pr(df))
+}
+
+plot_values <- plot_values[-1,]
+
+plot_values <- plot_values[complete.cases(plot_values),]
+colnames(plot_values) <- c("Recall", "Precision")
+
+if (nrow(plot_values > 0)){
+  plot(x=plot_values[,1], y=plot_values[,2],xlab="Recall",
+       ylab="Precision",type="b")
+} else{
+  cat("you messed up!")
+}
+
+#=========================================================
+
+plot_values <- data.frame(matrix(ncol=2))
+colnames(plot_values) <- c("V1", "V2")
+#ROC
+for (i in 0:17){
+  predict_elm = elm_predict(fit_elm, as.matrix(x_test))
+  threshold <- 0.1+i*0.05
+  predict_elm <- ifelse(predict_elm<threshold,0,1)
+  xtab <- table(y_test, predict_elm)
+  #print(as.matrix(xtab))
+  df <- as.data.frame(as.matrix(xtab))
+  plot_values <- rbind(plot_values,calc_roc(df))
+}
+plot_values <- plot_values[-1,]
+plot_values <- plot_values[complete.cases(plot_values),]
+colnames(plot_values) <- c("False_Pos", "True_Pos")
+
+if (nrow(plot_values > 0)){
+  plot(x=plot_values[,1], y=plot_values[,2],xlab="False Pos",
+       ylab="True Pos",type="b")
+} else{
+  cat("you messed up!")
+}
+
+
+calc_pr <- function(df){
+  #precision = true pos/(true pos + false pos)
+  #recall = true pos/(true pos + false neg)
+  if (any(df[,1]==1 && df[,2]==1)){
+    true_pos <- df[which(df[,1]==1&&df[,2]==1),3]
+  } else{
+    true_pos <- 0
+  }
+  
+  if (any(df[,1]==0 && df[,2]==1)){
+    false_pos <- df[which(df[,1]==0&&df[,2]==1),3]
+  } else{
+    false_pos <- 0
+  }
+  
+  if (any(df[,1]==1 && df[,2]==0)){
+    false_neg <- df[which(df[,1]==1&&df[,2]==0),3]
+  } else{
+    false_neg <- 0
+  }
+  precision <- df[4,3]/(df[4,3]+df[3,3])
+  recall <- df[4,3]/(df[4,3]+df[2,3])
+  as.data.frame(matrix(c(recall,precision),ncol=2))
+}
+
+calc_roc <- function(df){
+  if (any(df[,1]==1 && df[,2]==1)){
+    true_pos <- df[which(df[,1]==1&&df[,2]==1),3]
+  } else{
+    true_pos <- 0
+  }
+  
+  if (any(df[,1]==0 && df[,2]==1)){
+    false_pos <- df[which(df[,1]==0&&df[,2]==1),3]
+  } else{
+    false_pos <- 0
+  }
+  
+  if (any(df[,1]==1 && df[,2]==0)){
+    false_neg <- df[which(df[,1]==1&&df[,2]==0),3]
+  } else{
+    false_neg <- 0
+  }
+  
+  if (any(df[,1]==0 && df[,2]==0)){
+    true_neg <- df[which(df[,1]==0&&df[,2]==0),3]
+  } else{
+    true_neg <- 0
+  }
+  #false pos (x)= false pos/(false pos + true neg)
+  #true pos (y)= true pos/(true pos + false neg)
+  spec <- false_pos/(false_pos+true_neg)
+  sens <- true_pos/(true_pos + false_neg)
+  as.data.frame(matrix(c(spec,sens),ncol=2))
+}
 
 #linear =======================
 
@@ -38,8 +148,6 @@ rmse = function (y_true, y_pred) {
   out
 }
 
-y_test = data[251:nrow(data), predictVar]
-
 #mean square errors ====================
 
 print_results <- function(y_test, predict_elm, predict_lm){
@@ -47,6 +155,8 @@ print_results <- function(y_test, predict_elm, predict_lm){
   cat('the rmse error for linear-model is :', rmse(y_test, predict_lm), '\n')
   cat('range:', min(sqrt((y_test-predict_elm[,1])^2)), 'to', max(sqrt((y_test-predict_elm[,1])^2)))
 }
+
+
 
 print_results(y_test, predict_elm, predict_lm)
 
