7e1577a86ee8808889efdd25262386401128609d
hk21042 <hk21042@gmail.com>
2019-07-14 01:46:05 -0400

    safety push
    created a new file to get/combine new data (from feer.gsfc.nasa.gov). data includes fire power from terra and aqua satellites and the specific lat/long. code (should) get all locations from california/nevada and combine them all into one csv... might kill my computer so pushing to stay safe

---
+++ read_frp.R
@@ -0,0 +1,44 @@
+install.packages("readr")
+library("readr")
+
+files <- as.data.frame(as.matrix(list.files(
+  path = "/Users/hk/Desktop/Fire\ Downloads/006/MYD14", full.names = T)))
+files <- rbind(files, as.data.frame(as.matrix(list.files(
+  path = "/Users/hk/Desktop/Fire\ Downloads/006/MOD14", full.names = T))))
+
+
+data <- data.frame(matrix(ncol = 5))
+colnames(data) <- c("Date", "Time", "FP_latitude", "FP_longitude", "FP_power")
+
+for (j in 1:nrow(files)){
+  for (k in 1:length(list.files(path=toString(files[j,])))){
+    tbl <- read.table(toString(files[k,]),skip=6, header = FALSE,
+                      stringsAsFactors = FALSE,sep=",") 
+    colnames(tbl) <- as.matrix(tbl[1,])
+    tbl <- tbl[-1,c(1,2,6,7,17)]
+    rownames(tbl) <- 1:nrow(tbl)
+    
+    tbl$FP_latitude <- as.numeric(as.character(tbl$FP_latitude))
+    tbl$FP_longitude <- as.numeric(as.character(tbl$FP_longitude))
+    tbl$FP_power <- as.numeric(as.character(tbl$FP_power))
+    
+    i <- 1
+    while (i <= nrow(tbl)){
+      if (!(tbl$FP_longitude[i]>=-124 && tbl$FP_longitude[i]<=-114 && 
+            tbl$FP_latitude[i]>=32.5 && tbl$FP_latitude[i]<=40)){
+        tbl <- tbl[-i,]
+      } else{
+        i <- i+1
+      }
+    }
+    
+    data <- rbind(data, tbl)
+  }
+}
+
+write.csv(data, file="all_data.csv")
+
+
+
+
+
