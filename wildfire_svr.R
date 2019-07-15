020ec89a9ceb51b9a10123b1b4563dd207c0396c
kimh2 <Hannah.Kim@jhuapl.edu>
2019-07-15 16:08:32 -0400

    update svm, read_frp
    read_frp now plots correct density plots. directly compares active and inactive fire powers. pr/roc doesn't work on svm/svr, turned to using the tune function in e1071 instead. accuracy improves! since svr is still very inaccurate, looking into creating bins or smth to make prediction easier for the model. to do: continue with bins and start preparing for paper

--- wildfire_svr.R
+++ wildfire_svr.R
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 install.packages("e1071")
 library("e1071")
 library("caret")
 
 setwd("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data")
-data <- read.csv("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data/ml_dly_cal_r3.sel.csv")[,c(1:3,6,8,10,14,25,28,32,34:41)]
-# data <- data[c(which(data$month==6),which(data$month==7),which(data$month==8)),]
+data <- read.csv("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data/ml_dly_cal_r1.sel.csv")[,c(1:3,4:41)]
+data <- data[c(which(data$month==6),which(data$month==7),which(data$month==8)),]
 data <- data[order(data$year),]
 data <- data[,-c(1,2,3)]
 k <- 4
@@ -13,6 +12,19 @@ k <- 4
 set.seed(314)
 
 predictVar = which(names(data)=="fpc1")
+
+num_bins <- ceiling(range(data$fpc1)[2]/10)
+
+bin_split <- bin(unique(data$fpc1), nbins = num_bins, method = "length")
+bin_freq <- as.data.frame(table(bin_split))
+bin_names <- data.frame(matrix(ncol = 1))
+
+for (i in 1:nrow(bin_freq)){ #because apparently idk how to use sapply
+  bin_names <- rbind(bin_names,substr(bin_freq[i,1],2,lapply(strsplit(toString(
+    bin_freq[i,1]), ''), function(x) which(x == ',')-1)))
+}
+bin_names <- bin_names[-1,]
+bin_freq <- cbind(bin_names, bin_freq)
 
 data <- rbind(data[sample(which(data$fpc1==0),length(which(data$fpc1==0))/2),],data[which(data$fpc1!=0),])
 rows <- nrow(data)
@@ -46,48 +58,22 @@ for (i in 1:k){
   }
   cat("percent accuracy:", (right/total)*100, "\n")
   cat("percent error:", 100-(right/total)*100, "\n")
-}
-
-=======
-install.packages("e1071")
-library("e1071")
-library("caret")
-
-setwd("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data")
-data <- read.csv("C:/Users/kimh2/Desktop/Wildfire-NN-ML/ML_Data/ml_dly_cal_r3.sel.csv")[,1:41]
-data <- data[c(which(data$month==6),which(data$month==7),which(data$month==8)),]
-data <- data[order(data$year),]
-k <- 4
-
-num_train <- round(rows*(k-1)/k)
-set.seed(314)
-
-predictVar = which(names(data)=="fpc1")
-
-data <- rbind(data[sample(which(data$fpc1==0),length(which(data$fpc1==0))/2),],data[which(data$fpc1!=0),])
-rows <- nrow(data)
-row_per_fold <- nrow(data)/k
-
-s <- split(data,(sample(nrow(data), k, replace=F)))
-
-prev_index <- 1
-for (i in 1:k){
-  rand_index <- sample(nrow(s[[i]]),round(nrow(s[[i]])*(k-1)/k))
-  train <- data[row.names(s[[i]])[rand_index],]
-  test <- data[row.names(s[[i]])[-rand_index],]
   
-  svm1 <- svm(factor(fpc1) ~ ., data=train,
-              method="C-classification", kernal="radial", 
-              gamma=0.1, cost=10)
+  cat("====== tuning ====== \n")
   
-  summary(svm1)
+  svm1_tune <- tune(svm, train.x=train, train$fpc1, kernel="radial")
   
-  prediction <- predict(svm1, test)
-
-  xtab <- table(test$fpc1, prediction)
+  svm1_post_tune <- svm(factor(fpc1)~., data=train,type='C-classification',
+                        kernel = "radial")
+  summary(svm1_post_tune)
+  
+  prediction2 <- predict(svm1_post_tune, test)
+  
+  xtab <- table(test$fpc1, prediction2)
+  #print(as.matrix(xtab))
   df <- as.data.frame(as.matrix(xtab))
   right <- 0
-  total <- sum(df[,3])
+  total <- nrow(test)
   for (j in 1:nrow(df)){
     if (df[j,1]==df[j,2]){
       right <- right + df[j,3]
@@ -95,18 +81,5 @@ for (i in 1:k){
   }
   cat("percent accuracy:", (right/total)*100, "\n")
   cat("percent error:", 100-(right/total)*100, "\n")
+  cat("=============================================== \n")
 }
-# percent accuracy: 30.43478 
-# percent error: 69.56522 
-
-# percent accuracy: 26.08696 
-# percent error: 73.91304 
-
-# percent accuracy: 30.43478 
-# percent error: 69.56522 
-
-# percent accuracy: 21.73913 
-# percent error: 78.26087 
-
-#from least to greatest accuracy: r3, r2, r1
->>>>>>> 667077de0f78d4fe1c9ca5a44a203e6930559e3b
