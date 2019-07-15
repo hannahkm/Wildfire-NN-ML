1590aa2c15f6f810bbdec0bdb2242331eef5ed5c
hk21042 <hk21042@gmail.com>
2019-07-15 00:31:27 -0400

    read_frp.R updates (see descrip)
    the which() statement is STILL being a pain - keeps returning integer(0) for the longitude/latitude ranges even though i KNOW there should be rows. code ran and it took literally like 12 hours (lol) but it worked (thank god), so now I have all the data needed. Also started plotting and playing around with graphs (frequency plots of fire power (log 10). to do: for the sake of your sanity figure out the which function, find out how to adjust the density/frequency plots so they aren't normalized/fast fourier functioned/etc.

--- read_frp.R
+++ read_frp.R
@@ -1,5 +1,7 @@
-install.packages("readr")
+#install.packages("readr") - for reading multiple files
+#install.packages("OneR) - for creating bins from data
 library("readr")
+library("OneR")
 
 files <- as.data.frame(as.matrix(list.files(
   path = "/Users/hk/Desktop/Fire\ Downloads/006/MYD14", full.names = T)))
@@ -32,14 +34,62 @@ for (j in 1:nrow(files)){
         i <- i+1
       }
     }
+    # data[which(tbl$FP_longitude >= -124 & tbl$FP_longitude <= -114
+    #            & tbl$FP_latitude >= 32.5 & tbl$FP_latitude <= 40),]
     
     data <- rbind(data, tbl)
   }
+  print(j)
 }
 
-write.csv(data, file="all_data.csv")
+data <- data[-1,]
+data <- data[-which(data$FP_power==0),]
+write.csv(data, file="all_data.csv", row.names=FALSE)
+
+#======================================= SUMMER ONLY
+
+df_months <- sapply(data[,1],substring,6,7)
+data <- cbind(df_months, data)
+
+data_summer <- data[which(data$df_months %in% c("06", "07", "08")),]
+
+power_freq <- as.data.frame(table(log(data_summer$FP_power,10)))
+power_freq[,1] <- as.numeric(as.character(power_freq[,1]))
+
+plot(density(power_freq[,1]))
+
+#======================================= ACTIVE SUMMERS
+
+df_years <- sapply(data$Date,substring,1,4)
+data <- cbind(df_years, data)
+
+data_active <- data[which((data$df_years %in% c("2006", "2008", "2015", "2016"))
+                          & (data$df_months %in% c("06", "07", "08"))),]
+
+power_freq_act <- as.data.frame(table(log(data_active$FP_power,10)))
+power_freq_act[,1] <- as.numeric(as.character(power_freq_act[,1]))
+
+plot(hist(power_freq_act[,1]), col = "red")
+
+#======================================= INACTIVE SUMMERS
+
+data_inactive <- data[which((data$df_years %in% c("2005", "2007", "2010", "2011"))
+                          & (data$df_months %in% c("06", "07", "08"))),]
+
+power_freq_inact <- as.data.frame(table(log(data_inactive$FP_power,10)))
+power_freq_inact[,1] <- as.numeric(as.character(power_freq_inact[,1]))
+
+lines(hist(power_freq_inact[,1]), col = "blue")
 
 
 
 
+
+num_bins <- ceiling(range(power_freq[,1])[2])/0.05
+
+bin_split <- bin(power_freq[,1], nbins = num_bins, method = "length")
+bin_freq <- as.data.frame(table(bin_split))
+
+
+#substr(strsplit(toString(bin_split[1]),","),2,length(toString(bin_split[1])))
 
