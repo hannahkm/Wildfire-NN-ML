0d574b6a90634693082bd4f25a949ffcb1965a76
hk21042 <hk21042@gmail.com>
2019-07-31 00:28:24 -0400

    trying to create maps (see descrip)
    graphing maps in order to visualize regions in california/nevada. to do: figure out how to use the grids and spatialpolygonsdataframes

---
+++ graphing_maps.R
@@ -0,0 +1,20 @@
+data <- read.csv("/Users/hk/Desktop/Fire\ Downloads/all_data.csv")
+
+lat_max <- max(data$FP_latitude)
+lat_min <- min(data$FP_latitude)
+long_max <- max(data$FP_longitude)
+long_min <- min(data$FP_longitude)
+
+sqr_len <- 0.5
+
+lat_value <- lat_min
+long_value <- long_min
+
+sqr_values <- data.frame(matrix(ncol=1))
+while(lat_value < lat_max){
+  while (long_value < long_max){
+    df <- data[which(data$FP_latitude>=lat_value & data$FP_latitude<(lat_value+sqr_len) &
+                       data$FP_longitude>=long_value & data$FP_longitude<(long_value+sqr_len)),"FP_power"]
+    to_add <- data.frame(c(sum(df)))
+  }
+}
