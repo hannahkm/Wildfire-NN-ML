87d081cc3fdf9bf7880e8f68c8edc2b65a667283
hk21042 <hk21042@gmail.com>
2019-08-01 00:26:20 -0400

    updated graphing
    playing around with graphing maps. calculation of frp is done but now need to figure out how to translate that data to maps/color grid.

--- graphing_maps.R
+++ graphing_maps.R
@@ -10,15 +10,33 @@ sqr_len <- 0.5
 lat_value <- lat_min
 long_value <- long_min
 
-sqr_values <- data.frame(matrix(ncol=1))
+row_num <- round(lat_max-lat_min)/0.5
+col_num <- round(long_max-long_min)/0.5
+
+sqr_values <- data.frame(matrix(nrow = row_num, ncol=col_num))
+row_ind <- row_num
+col_ind <- 1
 while(lat_value < lat_max){
   while (long_value < long_max){
+    print(lat_value)
     df <- data[which(data$FP_latitude>=lat_value & 
                        data$FP_latitude<(lat_value+sqr_len) &
                        data$FP_longitude>=long_value & 
                        data$FP_longitude<(long_value+sqr_len)),"FP_power"]
-    to_add <- data.frame(c(sum(df)))
+    if (NROW(data)==0){
+      to_add <- 0
+    } else{
+      to_add <- sum(df)
+    }
+    sqr_values[row_ind, col_ind] <- to_add
+    
+    long_value <- long_value + sqr_len
+    col_ind <- col_ind + 1
   }
+  lat_value <- lat_value + sqr_len
+  long_value <- long_min
+  col_ind <- 1
+  row_ind <- row_ind - 1
 }
 
 
