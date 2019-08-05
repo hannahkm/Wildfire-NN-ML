469192daeea89ab9af346dc45fd1d52b59d66824
hk21042 <hk21042@gmail.com>
2019-08-05 00:46:21 -0400

    map completed (see descrip)
    graphing_maps.R is complete and accurate with fire power colors overlayed on map.

--- graphing_maps.R
+++ graphing_maps.R
@@ -5,13 +5,13 @@ lat_min <- min(data$FP_latitude)
 long_max <- max(data$FP_longitude)
 long_min <- min(data$FP_longitude)
 
-sqr_len <- 0.5
+sqr_len <- 0.1
 
 lat_value <- lat_min
 long_value <- long_min
 
-row_num <- round(lat_max-lat_min)/0.5
-col_num <- round(long_max-long_min)/0.5
+row_num <- round(lat_max-lat_min)/0.1
+col_num <- round(long_max-long_min)/0.1
 
 sqr_values <- data.frame(matrix(nrow = row_num, ncol=col_num))
 coord_values <- data.frame(matrix(ncol = 3))
@@ -30,7 +30,7 @@ while(lat_value < lat_max){
       to_add <- sum(df)
     }
     sqr_values[row_ind, col_ind] <- to_add
-    add_row <- as.data.frame(cbind(lat_value, long_value, to_add))
+    add_row <- as.data.frame(cbind(long_value, lat_value, to_add))
     colnames(add_row) <- colnames(coord_values)
     coord_values <- rbind(coord_values, add_row)
     
@@ -44,12 +44,49 @@ while(lat_value < lat_max){
 }
 
 coord_values <- coord_values[-1,]
+
+coord_values$value <- rev(heat.colors(len))[as.numeric(cut(
+  coord_values$value,breaks=len/0.1))]
 len <- NROW(unique(coord_values$value))
+par(pty="s")
 plot(x=coord_values[,1], y=coord_values[,2], 
-     col=rev(heat.colors(len))
-     [as.numeric(cut(coord_values$value,breaks=len/0.5))], pch=15, cex = 2.5,
-     xlim=c(lat_min,lat_max), ylim=c(long_min,long_max), tck=-.03,
+     col=coord_values[,3], 
+     pch=15, cex = 2.5,
+     xlim=c(long_min, long_max), ylim=c(lat_min, lat_max), tck=-.03,
      xlab="longitude", ylab="latitude", main="Fire Power in California")
 
+#=================
+#install.packages("ggmap")
+library("ggmap")
+library("maps") 
+library("mapdata")
+library("ggplot2")
+temp <- as.data.frame(coord_values) 
+states <- map_data("state")
+ca_nv <- subset(states, region %in% c("california", "nevada"))
 
+coord_values[which(coord_values$value=="#FFFFF5FF"),3] <- NA
+
+ggplot(data = ca_nv) + 
+  geom_polygon(aes(x = long, y = lat, group = group), 
+               fill = "white", color = "black") +
+  coord_fixed(1.3) + 
+  labs(title="Fire Power", x="Longitude", y="Latitude") +
+  theme(
+    axis.text = element_text(color="black", size=12),
+    plot.title = element_text(color="black", size=18, hjust=0.5),
+    axis.title.x = element_text(color="black", size=14),
+    axis.title.y = element_text(color="black", size=14)) +
+  geom_point(data = coord_values, 
+             mapping = aes(x = x, y = y, color=value), shape=15, size=2) +
+  scale_color_identity()
+
+palette(heat.colors(nrow(temp))) 
+for (lat in seq(lat_min, lat_max, 0.5)) {
+  for (lon in seq(long_min, long_max, 0.5)) { 
+    col <- temp[which(temp$x==lat & temp$y==lon), 3] 
+    if (!is.na(col)) polygon(lat, lon, col=col, border=NA) 
+  } 
+}
+palette("default") 
 
